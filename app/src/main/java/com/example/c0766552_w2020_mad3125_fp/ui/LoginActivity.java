9f88e25bf95f490b595167e9ded007d1ac58ad8c
NikitaSandhu0096 <sandhunikita0096@gmail.com>
2020-04-13 17:02:55 -0400

    [Nikita] - remember me

--- app/src/main/java/com/example/c0766552_w2020_mad3125_fp/ui/LoginActivity.java
+++ app/src/main/java/com/example/c0766552_w2020_mad3125_fp/ui/LoginActivity.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 
 import com.example.c0766552_w2020_mad3125_fp.R;
 import com.google.android.material.textfield.TextInputEditText;
@@ -18,6 +22,7 @@ public class LoginActivity extends AppCompatActivity {
     private TextInputEditText edtPassword;
     private CheckBox cbRemember;
     private Button btnLogin;
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,13 +33,30 @@ public class LoginActivity extends AppCompatActivity {
         edtPassword = findViewById(R.id.passwordTextInputEditText);
         cbRemember = findViewById(R.id.checkBox);
         btnLogin = findViewById(R.id.button);
+        sharedPreferences = getSharedPreferences("login", MODE_PRIVATE);
+        final SharedPreferences.Editor editor = sharedPreferences.edit();
+        edtEmail.setText(sharedPreferences.getString("email",""));
+        edtPassword.setText(sharedPreferences.getString("password",""));
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(edtEmail.getText().toString().equals("nsandhu@gmail.com") && edtPassword.getText().toString().equals("nsandhu")){
+                    String email = edtEmail.getText().toString();
+                    String password = edtPassword.getText().toString();
                     if (cbRemember.isChecked()){
-
+                        editor.putString("email",email);
+                        editor.putString("password",password);
+                        editor.apply();
+                        Toast.makeText(LoginActivity.this, "Checked", Toast.LENGTH_SHORT).show();;
+                        Intent intent = new Intent(LoginActivity.this, CustomerListActivity.class);
+                        startActivity(intent);
+                    }else {
+                        editor.clear();
+                        editor.apply();
+                        Toast.makeText(LoginActivity.this, "UnChecked", Toast.LENGTH_SHORT).show();;
+                        Intent intent = new Intent(LoginActivity.this, CustomerListActivity.class);
+                        startActivity(intent);
                     }
                 }else {
                     AlertDialog.Builder alert = new AlertDialog.Builder(LoginActivity.this);
@@ -58,6 +80,5 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
-
     }
 }
