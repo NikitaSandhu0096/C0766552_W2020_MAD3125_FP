37056e3856deba7ac08ca3197ad14039d8be6d0f
NikitaSandhu0096 <sandhunikita0096@gmail.com>
2020-04-14 00:36:01 -0400

    [Nikita] - Mobile class created

--- app/src/main/java/com/example/c0766552_w2020_mad3125_fp/model/Bill.java
+++ app/src/main/java/com/example/c0766552_w2020_mad3125_fp/model/Bill.java
@@ -1,17 +1,50 @@
 package com.example.c0766552_w2020_mad3125_fp.model;
 
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Bill {
-    String billId;
-    String billDate;
-    String billType;
-    Float totalBillAmount;
+    private String billId;
+    private String billDate;
+    private String billType;
+    private Double totalBillAmount;
 
-    public Bill(String billId, String billDate, String billType, Float totalBillAmount) {
+    public Bill(String billId, String billDate, String billType, Double totalBillAmount) {
         this.billId = billId;
         this.billDate = billDate;
         this.billType = billType;
+        this.totalBillAmount = totalBillAmount;
+    }
+
+    public String getBillId() {
+        return billId;
+    }
+
+    public void setBillId(String billId) {
+        this.billId = billId;
+    }
+
+    public String getBillDate() {
+        return billDate;
+    }
+
+    public void setBillDate(String billDate) {
+        this.billDate = billDate;
+    }
+
+    public String getBillType() {
+        return billType;
+    }
+
+    public void setBillType(String billType) {
+        this.billType = billType;
+    }
+
+    public Double getTotalBillAmount() {
+        return totalBillAmount;
+    }
+
+    public void setTotalBillAmount(Double totalBillAmount) {
         this.totalBillAmount = totalBillAmount;
     }
 }
---
+++ app/src/main/java/com/example/c0766552_w2020_mad3125_fp/model/Mobile.java
@@ -0,0 +1,58 @@
+package com.example.c0766552_w2020_mad3125_fp.model;
+
+public class Mobile extends Bill  {
+    private String mobileManufacturer;
+    private String planName;
+    private String mobileNumber;
+    private int internetGBUsed;
+    private int minuteUsed;
+
+    public Mobile(String billId, String billDate, String billType, Double totalBillAmount, String mobileManufacturer, String planName, String mobileNumber, int internetGBUsed, int minuteUsed) {
+        super(billId, billDate, billType, totalBillAmount);
+        this.mobileManufacturer = mobileManufacturer;
+        this.planName = planName;
+        this.mobileNumber = mobileNumber;
+        this.internetGBUsed = internetGBUsed;
+        this.minuteUsed = minuteUsed;
+    }
+
+    public String getMobileManufacturer() {
+        return mobileManufacturer;
+    }
+
+    public void setMobileManufacturer(String mobileManufacturer) {
+        this.mobileManufacturer = mobileManufacturer;
+    }
+
+    public String getPlanName() {
+        return planName;
+    }
+
+    public void setPlanName(String planName) {
+        this.planName = planName;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public int getInternetGBUsed() {
+        return internetGBUsed;
+    }
+
+    public void setInternetGBUsed(int internetGBUsed) {
+        this.internetGBUsed = internetGBUsed;
+    }
+
+    public int getMinuteUsed() {
+        return minuteUsed;
+    }
+
+    public void setMinuteUsed(int minuteUsed) {
+        this.minuteUsed = minuteUsed;
+    }
+}
