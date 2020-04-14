9ced8545646e6969d640c27a13a32e45649bc586
NikitaSandhu0096 <sandhunikita0096@gmail.com>
2020-04-14 01:48:30 -0400

    [Nikita] - Hydro class created

---
+++ app/src/main/java/com/example/c0766552_w2020_mad3125_fp/model/Hydro.java
@@ -0,0 +1,28 @@
+package com.example.c0766552_w2020_mad3125_fp.model;
+
+public class Hydro extends Bill {
+    private String agencyName;
+    private int unitConsumed;
+
+    public Hydro(String billId, String billDate, String billType, Double totalBillAmount, String agencyName, int unitConsumed) {
+        super(billId, billDate, billType, totalBillAmount);
+        this.agencyName = agencyName;
+        this.unitConsumed = unitConsumed;
+    }
+
+    public String getAgencyName() {
+        return agencyName;
+    }
+
+    public void setAgencyName(String agencyName) {
+        this.agencyName = agencyName;
+    }
+
+    public int getUnitConsumed() {
+        return unitConsumed;
+    }
+
+    public void setUnitConsumed(int unitConsumed) {
+        this.unitConsumed = unitConsumed;
+    }
+}
