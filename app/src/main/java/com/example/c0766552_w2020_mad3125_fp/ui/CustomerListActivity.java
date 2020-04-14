a9a52ab38e3216036ed6d27a604630b08e006504
NikitaSandhu0096 <sandhunikita0096@gmail.com>
2020-04-13 20:09:10 -0400

    [Nikita] - customer list title

--- app/src/main/java/com/example/c0766552_w2020_mad3125_fp/ui/CustomerListActivity.java
+++ app/src/main/java/com/example/c0766552_w2020_mad3125_fp/ui/CustomerListActivity.java
@@ -1,5 +1,6 @@
 package com.example.c0766552_w2020_mad3125_fp.ui;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -12,5 +13,8 @@ public class CustomerListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_list);
+
+        ActionBar actBar = getSupportActionBar();
+        actBar.setTitle("Customer List");
     }
 }
