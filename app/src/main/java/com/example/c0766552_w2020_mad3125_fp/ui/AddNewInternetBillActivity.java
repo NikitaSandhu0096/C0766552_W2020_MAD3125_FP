53e82474a7265e274a354ff9af51a8cbf88e0828
NikitaSandhu0096 <sandhunikita0096@gmail.com>
2020-04-15 18:31:09 -0400

    [Nikita] - internet bill date

--- app/src/main/java/com/example/c0766552_w2020_mad3125_fp/ui/AddNewInternetBillActivity.java
+++ app/src/main/java/com/example/c0766552_w2020_mad3125_fp/ui/AddNewInternetBillActivity.java
@@ -3,13 +3,19 @@ package com.example.c0766552_w2020_mad3125_fp.ui;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 
 import com.example.c0766552_w2020_mad3125_fp.R;
 import com.google.android.material.textfield.TextInputEditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class AddNewInternetBillActivity extends AppCompatActivity {
 
@@ -29,6 +35,25 @@ public class AddNewInternetBillActivity extends AppCompatActivity {
 
         edtIBillID = findViewById(R.id.internetBillIDTextInputEditText);
         edtIBillDate = findViewById(R.id.internetBillDateTextInputEditText);
+
+        edtIBillDate.setOnClickListener(new View.OnClickListener() {        //https://www.journaldev.com/9976/android-date-time-picker-dialog
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog dialog = new DatePickerDialog(AddNewInternetBillActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {       //https://android--code.blogspot.com/2015/08/android-datepickerdialog-date-format.html
+                        Calendar calendar = Calendar.getInstance();
+                        calendar.set(year, month, dayOfMonth);
+                        Date date = calendar.getTime();
+                        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MMM-yyyy");
+                        String s = dateFormat.format(date);
+                        edtIBillDate.setText(s);
+                    }
+                }, Calendar.getInstance().get(Calendar.YEAR), Calendar.getInstance().get(Calendar.MONTH), Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
+                dialog.show();
+            }
+        });
+
         edtProviderName = findViewById(R.id.internetProviderNameTextInputEditText);
         edtIInternetUsage = findViewById(R.id.internetInternetUsageTextInputEditText);
         btnISave = findViewById(R.id.internetbutton);
