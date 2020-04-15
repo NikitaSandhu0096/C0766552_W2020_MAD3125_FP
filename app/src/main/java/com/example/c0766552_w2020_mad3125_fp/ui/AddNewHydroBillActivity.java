8195434a578c2567811a9246148cae8a138250a4
NikitaSandhu0096 <sandhunikita0096@gmail.com>
2020-04-15 02:20:46 -0400

    [Nikita] - hydro ids

--- app/src/main/java/com/example/c0766552_w2020_mad3125_fp/ui/AddNewHydroBillActivity.java
+++ app/src/main/java/com/example/c0766552_w2020_mad3125_fp/ui/AddNewHydroBillActivity.java
@@ -4,10 +4,18 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.c0766552_w2020_mad3125_fp.R;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class AddNewHydroBillActivity extends AppCompatActivity {
+
+    private TextInputEditText edtHBillID;
+    private TextInputEditText edtHBillDate;
+    private TextInputEditText edtAgencyName;
+    private TextInputEditText edtUnitConsumed;
+    private Button btnHSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +24,12 @@ public class AddNewHydroBillActivity extends AppCompatActivity {
 
         ActionBar actBar = getSupportActionBar();
         actBar.setTitle("Add New Hydro Bill");
+
+        edtHBillID = findViewById(R.id.hydroBillIDTextInputEditText);
+        edtHBillDate = findViewById(R.id.hydroBillDateTextInputEditText);
+        edtAgencyName = findViewById(R.id.hydroAgencyNameTextInputEditText);
+        edtUnitConsumed = findViewById(R.id.hydroUnitConsumedTextInputEditText);
+
+        
     }
 }
