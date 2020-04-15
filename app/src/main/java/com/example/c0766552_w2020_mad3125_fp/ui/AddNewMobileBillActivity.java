5875c0b5f971ceb85d723cf28d57c30015ab671c
NikitaSandhu0096 <sandhunikita0096@gmail.com>
2020-04-15 02:03:43 -0400

    [Nikita] - mobile title

--- app/src/main/java/com/example/c0766552_w2020_mad3125_fp/ui/AddNewMobileBillActivity.java
+++ app/src/main/java/com/example/c0766552_w2020_mad3125_fp/ui/AddNewMobileBillActivity.java
@@ -1,5 +1,6 @@
 package com.example.c0766552_w2020_mad3125_fp.ui;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -12,5 +13,8 @@ public class AddNewMobileBillActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_mobile_bill);
+
+        ActionBar actBar = getSupportActionBar();
+        actBar.setTitle("Add New Mobile Bill");
     }
 }
