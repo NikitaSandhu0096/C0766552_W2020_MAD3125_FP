19106fd5cf9926370a592fa85056eb6bc375c678
NikitaSandhu0096 <sandhunikita0096@gmail.com>
2020-04-15 02:39:32 -0400

    [Nikita] - mobile ids

--- app/src/main/java/com/example/c0766552_w2020_mad3125_fp/ui/AddNewMobileBillActivity.java
+++ app/src/main/java/com/example/c0766552_w2020_mad3125_fp/ui/AddNewMobileBillActivity.java
@@ -4,10 +4,21 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.c0766552_w2020_mad3125_fp.R;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class AddNewMobileBillActivity extends AppCompatActivity {
+
+    private TextInputEditText edtMBillID;
+    private TextInputEditText edtMBillDate;
+    private TextInputEditText edtManufacturerName;
+    private TextInputEditText edtPlanName;
+    private TextInputEditText edtMobileNumber;
+    private TextInputEditText edtMInternetUsage;
+    private TextInputEditText edtMinutesUsage;
+    private Button btnMSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +27,14 @@ public class AddNewMobileBillActivity extends AppCompatActivity {
 
         ActionBar actBar = getSupportActionBar();
         actBar.setTitle("Add New Mobile Bill");
+
+        edtMBillID = findViewById(R.id.mobileBillIDTextInputEditText);
+        edtMBillDate = findViewById(R.id.mobileBillDateTextInputEditText);
+        edtManufacturerName = findViewById(R.id.mobileManufacturerNameTextInputEditText);
+        edtPlanName = findViewById(R.id.mobilePlanNameTextInputEditText);
+        edtMobileNumber = findViewById(R.id.mobileMobileNumberTextInputEditText);
+        edtMInternetUsage = findViewById(R.id.mobileInternetUsageTextInputEditText);
+        edtMinutesUsage = findViewById(R.id.mobileMinutesUsageTextInputEditText);
+        btnMSave = findViewById(R.id.mobilebutton);
     }
 }
