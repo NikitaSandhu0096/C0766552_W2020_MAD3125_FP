94c187fd56e4eacc357dc60901ec8c6626061887
NikitaSandhu0096 <sandhunikita0096@gmail.com>
2020-04-15 02:26:01 -0400

    [Nikita] - hydro errors

--- app/src/main/java/com/example/c0766552_w2020_mad3125_fp/ui/AddNewHydroBillActivity.java
+++ app/src/main/java/com/example/c0766552_w2020_mad3125_fp/ui/AddNewHydroBillActivity.java
@@ -3,7 +3,9 @@ package com.example.c0766552_w2020_mad3125_fp.ui;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.example.c0766552_w2020_mad3125_fp.R;
@@ -29,7 +31,29 @@ public class AddNewHydroBillActivity extends AppCompatActivity {
         edtHBillDate = findViewById(R.id.hydroBillDateTextInputEditText);
         edtAgencyName = findViewById(R.id.hydroAgencyNameTextInputEditText);
         edtUnitConsumed = findViewById(R.id.hydroUnitConsumedTextInputEditText);
+        btnHSave = findViewById(R.id.hydrobutton);
 
-        
+        btnHSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String hID = edtHBillID.getText().toString().trim();
+                String hBillDate = edtHBillDate.getText().toString().trim();
+                String agencyName = edtAgencyName.getText().toString().trim();
+                String unitConsumed = edtUnitConsumed.getText().toString().trim();
+
+                if(hID.isEmpty()){
+                    edtHBillID.setError("Please enter Bill ID");
+                } else if(hBillDate.isEmpty()){
+                    edtHBillDate.setError("Please enter Bill Date");
+                } else if(agencyName.isEmpty()){
+                    edtAgencyName.setError("Please enter Agency Name");
+                } else if(unitConsumed.isEmpty()){
+                    edtUnitConsumed.setError("Please enter Unit Consumed");
+                } else {
+                    Intent hint = new Intent(AddNewHydroBillActivity.this, CustomerDetailsActivity.class);
+                    startActivity(hint);
+                }
+            }
+        });
     }
 }
