a59286471160d15fb79d1f5459283378ffc7969f
NikitaSandhu0096 <sandhunikita0096@gmail.com>
2020-04-15 01:26:21 -0400

    [Nikita] - customer details

--- app/src/main/java/com/example/c0766552_w2020_mad3125_fp/ui/CustomerDetailsActivity.java
+++ app/src/main/java/com/example/c0766552_w2020_mad3125_fp/ui/CustomerDetailsActivity.java
@@ -3,14 +3,32 @@ package com.example.c0766552_w2020_mad3125_fp.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.c0766552_w2020_mad3125_fp.R;
+import com.example.c0766552_w2020_mad3125_fp.model.Customer;
 
 public class CustomerDetailsActivity extends AppCompatActivity {
+
+    private TextView txtCustomerId;
+    private TextView txtFullName;
+    private TextView txtEmail;
+    private TextView txtTotalAmountToPay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_details);
+
+        txtCustomerId = findViewById(R.id.textView5);
+        txtFullName = findViewById(R.id.textView6);
+        txtEmail = findViewById(R.id.textView7);
+        txtTotalAmountToPay = findViewById(R.id.textView8);
+
+        Customer tempobj = (Customer) getIntent().getSerializableExtra("customers");
+
+        txtCustomerId.setText(tempobj.getCustomerId());
+        txtFullName.setText(tempobj.getFullName());
+        txtEmail.setText(tempobj.getEmail());
     }
 }
