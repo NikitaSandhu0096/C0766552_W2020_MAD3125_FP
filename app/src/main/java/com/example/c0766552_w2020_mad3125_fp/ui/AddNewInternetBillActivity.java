404917eb857e84645642c2e68bc50a531ebd846f
NikitaSandhu0096 <sandhunikita0096@gmail.com>
2020-04-15 02:29:20 -0400

    [Nikita] - internet ids

--- app/src/main/java/com/example/c0766552_w2020_mad3125_fp/ui/AddNewInternetBillActivity.java
+++ app/src/main/java/com/example/c0766552_w2020_mad3125_fp/ui/AddNewInternetBillActivity.java
@@ -4,10 +4,18 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.c0766552_w2020_mad3125_fp.R;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class AddNewInternetBillActivity extends AppCompatActivity {
+
+    private TextInputEditText edtIBillID;
+    private TextInputEditText edtIBillDate;
+    private TextInputEditText edtProviderName;
+    private TextInputEditText edtIInternetUsage;
+    private Button btnISave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +24,13 @@ public class AddNewInternetBillActivity extends AppCompatActivity {
 
         ActionBar actBar = getSupportActionBar();
         actBar.setTitle("Add New Internet Bill");
+
+        edtIBillID = findViewById(R.id.internetBillIDTextInputEditText);
+        edtIBillDate = findViewById(R.id.internetBillDateTextInputEditText);
+        edtProviderName = findViewById(R.id.internetProviderNameTextInputEditText);
+        edtIInternetUsage = findViewById(R.id.internetInternetUsageTextInputEditText);
+        btnISave = findViewById(R.id.internetbutton);
+
+        
     }
 }
